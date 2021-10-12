b9f061612ae754d2ff9af2b86f4f50ecf8c2a232
gabrisca <gabrielescarparo1985@gmail.com>
2021-10-12 13:43:04 +0200

    HAMBURGER MENU OK

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,32 +1,46 @@
+//////////////////////////////////////////////////////
+
 // * access to an element
 let id = (id) => document.getElementById(id);
 let classes = (classes) => document.getElementsByClassName(classes);
 let query = (query) => document.querySelector(query);
+let queryAll = (query) => document.querySelectorAll(query);
+
+//////////////////////////////////////////////////////
+
+//////////////////////////////////////////////////////
 
 // * Selectors
-const myLinks = classes('link');
-// const openDropdown = classes("with_dropdown");
-// const dropdown = classes("dropdown");
-const chevron = query('i');
+// DROPDOWN MENU
+const myLinks = classes('link'),
+      chevron = query('i');
 
+// HAMBURGER MENU
+const menuItems = queryAll(".menuItem"),
+      menu = query(".hamburger_menu"),
+      hamburger = query(".hamburger"),
+      closeIcon= query(".my_closed"),
+      menuIcon = query(".my_hamburger");
+
+//////////////////////////////////////////////////////
+
+// * Events
+
+// HAMBURGER MENU
+hamburger.addEventListener("click", toggleMenu);
+menuItems.forEach( 
+  function(menuItem) { 
+    menuItem.addEventListener("click", toggleMenu);
+  }
+)
+
+//////////////////////////////////////////////////////
 
 // * Functions
 
-// funzione che aggiunge e rimuove una classe all'hover sui links
-function addClassToLinks(links, classe) {
-  for (var i = 0; i < links.length; i++) {
-    links[i].addEventListener("mouseenter", function () {
-      this.classList.add(classe)
-    });
-    links[i].addEventListener("mouseleave", function () {
-      this.classList.remove(classe)
-    });
-  }
-}
-
+// DROPDOWN MENU
 // funzione che, quando viene richiamata onclick, aggiunge e toglie la classe show e ruota lo chevron
 function myDropdown(idEl) {
-  console.log('click');
   this.document.getElementById(idEl).classList.toggle("show");
   chevron.classList.toggle("rotate")
 }
@@ -41,3 +55,21 @@ window.onclick = function (e) {
     }
   }
 }
+
+// HAMBURGER MENU
+// funzione che al click aggiunge e toglie le classi showMenu e hideMenu
+function toggleMenu() {
+  if (menu.classList.contains("showMenu")) {
+    menu.classList.remove("showMenu");
+    menu.classList.add("hideMenu");
+    closeIcon.style.display = "none";
+    menuIcon.style.display = "block";
+  } else {
+    menu.classList.remove("hideMenu");
+    menu.classList.add("showMenu");
+    closeIcon.style.display = "block";
+    menuIcon.style.display = "none";
+  }
+}
+
+
